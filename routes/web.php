d772007c93c6c4fe70933cbf9b20c335025066e5
zhin hawkar <zhinhawkar@gmail.com>
2025-11-19 21:38:39 +0300

    migrations fixed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/run-storage-link', function () {
+    Artisan::call('storage:link');
+    return "Storage link created!";
+});
+Route::get('/clear-route', function () {
+    Artisan::call('route:clear');
+    return "Route Cleared Successfuly!";
 });
